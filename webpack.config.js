5a984630a35481cbc2264b945cbebbc641bc3925
Gideon Baik <gideonbaik@gmail.com>
2018-02-13 21:40:46 -0800

    Init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/src');
+var DIST_DIR = path.join(__dirname, '/dist');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    bundle: `${SRC_DIR}/index.js`
+  },
+  output: {
+    filename: '[name].js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',      
+        include: SRC_DIR,
+        test: /\.jsx?/,
+        query: {
+          presets: ['react', 'es2015', 'stage-3']
+        }
+      }
+    ]
+  }
+};
