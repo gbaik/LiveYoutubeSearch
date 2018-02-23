9308cb85645bc90ee124c9829042c7d3530c4875
Gideon Baik <gideonbaik@gmail.com>
2018-02-22 18:09:06 -0800

    Add react-router

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import VideoPlayer from "../containers/VideoPlayer.js";
+import Search from '../containers/Search.js';
+import VideoPlayer from '../containers/VideoPlayer.js';
+import VideoList from '../containers/VideoList.js';
 
 const Display = ({}) => (
   <div>
-    <VideoPlayer />
+    <Search />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <VideoPlayer />
+        </Route>
+        <Route path="/search">
+          <VideoList />
+        </Route>      
+      </Switch>
+    </BrowserRouter>
   </div>
 );
 
---
+++ src/containers/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = ({}) => (
+  <div>
+    Hello World
+  </div> 
+);
+
+export default Search;
