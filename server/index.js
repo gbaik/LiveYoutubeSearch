96b56b41f01ccded125c188b3d82b63beb9ef536
Gideon Baik <gideonbaik@gmail.com>
2018-02-23 02:10:08 -0800

    Add temporary fix to client and server side rendering

--- server/index.js
+++ server/index.js
@@ -5,15 +5,20 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static('./'));
 app.use(express.static(path.join(__dirname, '/../dist')));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 });
 
+app.get('/search', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/../dist/index.html'));  
+})
+
 app.get('/results', function (req, res) {
   const searchQuery = req.query.search_query;
-  
+  console.log('hit');
   request.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&q=${searchQuery}&key=${process.env.youtubeAPI}`, function (error, response, body) {
     if (error) {
       throw error;
