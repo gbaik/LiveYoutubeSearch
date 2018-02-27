003ac69df9834356d085f718f997f6187e200c19
Gideon Baik <gideonbaik@gmail.com>
2018-02-26 22:22:43 -0800

    Fix environment variables for session storage

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,10 @@ const RedisStore = require('connect-redis')(session);
 const url = require('url');
 const redis = require('redis');
 
-const params = url.parse(process.env.REDIS_URL);
+const params = process.env.REDIS_URL ? url.parse(process.env.REDIS_URL) : null;
+const host = params ? params.hostname : 'localhost';
+const port = params ? params.port : 6379;
+
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -13,11 +16,14 @@ module.exports.verify = (req, res, next) => {
   }
 };
 
+console.log('host:', host);
+console.log('port:', port);
+
 module.exports.session = session({
   store: new RedisStore({
     client: redis.createClient(),
-    host: params.hostname || 'localhost',
-    port: params.port || 6379
+    host: host,
+    port: port
   }),
   secret: process.env.SESSION_SECRET,
   resave: false,
